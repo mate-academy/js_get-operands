4af6504ca0360f06887abf1838accbc8ec838a7e
Serhii <gespenst.serge@yandex.ru>
2019-09-25 20:15:21 +0300

    solution

--- src/getOperands.js
+++ src/getOperands.js
@@ -26,7 +26,9 @@
  * @return {string} - operands
  */
 function getOperands(str) {
-  // write code here
+  const operandsList = str.match(/-?\d\.?\d*/g);
+
+  return `a: ${operandsList[0]}, b: ${operandsList[1]}`;
 }
 
 module.exports = getOperands;
