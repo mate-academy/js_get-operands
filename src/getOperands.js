d696a078b6a2da1b02f7c1524a80122946c17bf5
Oles Marola <oles_pw@ukr.net>
2019-11-13 12:25:42 +0200

    get Operands

--- src/getOperands.js
+++ src/getOperands.js
@@ -26,7 +26,8 @@
  * @return {string} - operands
  */
 function getOperands(str) {
-  // write code here
+  const operands = str.replace(/[+\-*/]\s/g, '').split(' ');
+  return `a: ${operands[0]}, b: ${operands[1]}`;
 }
 
 module.exports = getOperands;
