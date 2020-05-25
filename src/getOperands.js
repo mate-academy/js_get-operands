e20cb6f7159a4b9c83dea1709d4e678b634a5c39
Valentyn Krushynin <krushunin89@gmail.com>
2020-05-25 14:20:26 +0300

    solution

--- src/getOperands.js
+++ src/getOperands.js
@@ -26,7 +26,11 @@
  * @return {string} - operands
  */
 function getOperands(str) {
-  // write code here
+  const pattern = new RegExp(/\s[+*-/]\s/);
+
+  const operands = str.split(pattern);
+
+  return `a: ${operands[0]}, b: ${operands[1]}`;
 }
 
 module.exports = getOperands;
