0bc72f4baaf20859b3d3890c8fc0ffc50e686e98
Liliia <chuiko1112@gmail.com>
2020-05-26 18:15:32 +0300

    fixed

--- src/getOperands.js
+++ src/getOperands.js
@@ -26,9 +26,9 @@
  * @return {string} - operands
  */
 function getOperands(str) {
-  const operands = str.match(/(-?)(\d{1,})(\.?)((\d{1,})?)/g);
+  const pattern = /(-?)(\d{1,})(\.?)((\d{1,})?)/g;
+  const operands = str.match(pattern);
 
-  return 'a: ' + operands[0] + ', b: ' + operands[1];
+  return `a: ` + operands[0] + `, b: ` + operands[1];
 }
-
 module.exports = getOperands;
